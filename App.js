89fd2e420df80b17d5d76933bcb9b4e00823e39c
Kyle Mendyk <kylem@fes.org>
2020-01-14 12:23:17 -0600

    Update App.js
    Change onSearchSubmit to be arrow function to auto-bind 'this'

--- App.js
+++ App.js
@@ -17,8 +17,24 @@ class App extends React.Component {
     //         console.log(response.data.results);
     //     });
     // }
+    state = { images: [] };
 
-    async onSearchSubmit(term) {
+    // // this will break on this.setState because this function is passed into SearchBar as a prop, so 'this' becomes props inside of SearchBar.js
+    // // need to rewrite it as an arrow function to auto-bind 'this'
+    // async onSearchSubmit(term) {
+    //     const response = await axios.get('https://api.unsplash.com/search/photos', {
+    //         params: { query: term },
+    //         headers: {
+    //             Authorization: 'Client-ID 67bb72b93646f63e54e8a8522cad20a26936170426016056d4eeb86faa3f78c2' 
+    //         }
+    //     });
+
+    //     console.log(this);
+    //     // console.log(response.data.results);
+    //     this.setState({ images: response.data.results });
+    // }
+
+    onSearchSubmit = async (term) => {
         const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
@@ -26,13 +42,16 @@ class App extends React.Component {
             }
         });
 
-        console.log(response.data.results);
+        // console.log(this);
+        // console.log(response.data.results);
+        this.setState({ images: response.data.results });
     }
     
     render() {
         return (
             <div className="ui container" style={{ marginTop: '10px'}}>
                 <SearchBar onSubmit={this.onSearchSubmit} />
+                Found: {this.state.images.length} images
             </div>
         );
     }
